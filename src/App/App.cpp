eac3ea44bdd1fefe84f1c8a4dd825bcd0a71b8a3
Benjamin Beilharz <benjamin.beilharz@pm.me>
2023-12-06 00:18:57 +0800

    Initial commit

---
+++ src/App/App.cpp
@@ -0,0 +1,13 @@
+﻿// komorebi.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "Komorebi.h"
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
